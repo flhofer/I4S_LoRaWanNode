273d2078aa2fb4ea3f451229b4208542755547de
Florian Hofer <florian.hofer@stud-inf.unibz.it>
2020-11-04 09:12:22 +0100

    Main, add setting definitions

--- main.h
+++ main.h
@@ -1,22 +1,27 @@
-// Only modify this file to include
-// - function definitions (prototypes)
-// - include files
-// - extern variable definitions
-// In the appropriate section
+/*
+ * main.h
+ *
+ *  Created on: May 25, 2020
+ *      Author: Florian Hofer
+ */
 
-#ifndef _LoRaWanNode_H_
-#define _LoRaWanNode_H_
-#include "Arduino.h"
-//add your includes for the project LoRaWanNode here
+#ifndef _NOILoraSendAPB_H_
+#define _NOILoraSendAPB_H_
 
+#include "Arduino.h" // Arduino Framework CORE -- I/O & Serial ports
 
-//end of add your includes here
+// Serial connection definition
+#define debugSerial Serial		// USB Serial
+#define loraSerial Serial1		// Hardware serial
 
+// EUI TTN Uno, ATmega32u4 0004A30B0021DD46
+// DevAddr, NwkSKey, AppSKey and the frequency plan
+#define LORA_DEVADDR	"01234567"
+#define LORA_NWSKEY		"01234567890abcdef01234567890abcd"
+#define LORA_APSKEY		"01234567890abcdef01234567890abcd"
 
-//add your function definitions for the project LoRaWanNode here
-
-
-
+//global variable declarations
+extern int debug;				// Global flag Debug console attached (PC)
 
 //Do not add code below this line
-#endif /* _LoRaWanNode_H_ */
+#endif /* _NOILoraSendAPB_H_ */
