913febc8a2eaab138967ff2578ba4c665d03051f
Florian Hofer <info@florianhofer.it>
2020-12-01 15:01:28 +0100

    Update limit resource freeing

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 #define UNCF_POLL	5			// How many times to poll
 #define TST_RETRY	5			// How many times retry to send message
 #define TST_MXRSLT	40			// What's the max number of test results we allow?
-#define RESFREEDEL	30000		// ~resource freeing delay ETSI requirement air-time reduction
+#define RESFREEDEL	40000		// ~resource freeing delay ETSI requirement air-time reduction
 
 // Allocate initPorts in init section3 of code
 void initPorts (void) __attribute__ ((naked)) __attribute__ ((section (".init3")));
