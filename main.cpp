7febfe5bf2b4f4356d059a52e236db0398eb65ca
Florian Hofer <info@florianhofer.it>
2021-07-07 10:12:43 +0200

    main update print format

--- main.cpp
+++ main.cpp
@@ -144,9 +144,11 @@ printTestResults(int count){
 
 	printPrgMem(PRTSTTTBL, PRTSTTRESULTS);
 	for (int i = 1; i<= min(TST_MXRSLT, count); i++, trn++){
-		sprintf(buf, "%02d;%07lu;%02u;%07lu;%07lu;0x%02X;%lu;%02u;%02d;%03d;%03d",
-				i, trn->testTime,
-				trn->txCount, trn->timeTx, trn->timeRx,
+		sprintf(buf, "%02d;%07lu;%02u;%6lu.%3u;%6lu.%3u;%6lu.%3u;0x%02X;%lu;%02u;%02d;%03d;%03d",
+				i, trn->testTime, trn->txCount,
+				trn->timeTx/1000,	(uint16_t)trn->timeTx%1000,
+				trn->timeRx/1000,	(uint16_t)trn->timeRx%1000,
+				trn->timeToRx/1000, (uint16_t)trn->timeToRx%1000,
 				trn->chnMsk, trn->txFrq, trn->txDR, trn->txPwr,
 				trn->rxRssi, trn->rxSnr);
 		debugSerial.println(buf);
